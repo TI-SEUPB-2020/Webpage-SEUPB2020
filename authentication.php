ccfac112165c1a02057ca45f2eccf941da0c9eea
jordiugarte <jordi933@gmail.com>
2020-05-27 18:26:27 -0400

    Update

--- authentication.php
+++ authentication.php
@@ -12,10 +12,10 @@ require 'db_connection.php';
 session_start();
 $message = '';
 
-if (!empty($_POST['idCode']) && !empty($_POST['name']) && !empty($_POST['lastName'])) {
-  $idCode = $_POST['idCode'];
-  $name = $_POST['name'];
-  $lastName = $_POST['lastName'];
+if (!empty($_GET['idCode']) && !empty($_GET['name']) && !empty($_GET['lastName'])) {
+  $idCode = $_GET['idCode'];
+  $name = $_GET['name'];
+  $lastName = $_GET['lastName'];
 
   $query = "SELECT COUNT(*) as count from registered_students where code = '$idCode' and full_name = '$lastName $name'";
   $consulta = mysqli_query($db_connection, $query);
@@ -41,9 +41,44 @@ if (!empty($_POST['idCode']) && !empty($_POST['name']) && !empty($_POST['lastNam
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verificación</title>
     <link rel="icon" type="image/png" href="res/favicon.png">
     <link rel="stylesheet" href="stylesheet.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style>
+		body {
+			background-image: url(res/authentication.jpg);
+			background-position: center center;
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+			background-size: cover;
+			background-color: #464646;  
+		}
+		.center_div{
+		    margin: 0 auto;
+		    width:80%;
+		    height: auto;
+		    background-color: #8906FF;
+		    padding: 20px;
+		    border-radius: 10px;
+		}
+		label {
+		    cursor: pointer;
+		    color: #ffffff;
+		    display: block;
+		    padding: 5px;
+		    margin: 3px;
+		}
+		.center {
+		  display: block;
+		  margin-left: auto;
+		  margin-right: auto;
+		  width: 50%; 
+		  height: auto;
+		}
+    </style>
   </head>
   <body>
 
@@ -52,12 +87,28 @@ if (!empty($_POST['idCode']) && !empty($_POST['name']) && !empty($_POST['lastNam
       echo "<h1>$message</h1>";
     }
     ?>
-
-    <form action="authentication.php" method="post">
-      <input type="text" name="idCode" value="" placeholder="Código">
-      <input type="text" name="name" value="" placeholder="Nombre">
-      <input type="text" name="lastName" value="" placeholder="Apellido">
-      <button type="submit" name="button">Login</button>
-    </form>
+    <div class="container">
+    	<img src="res/logo.png" class="center">
+    </div>
+    <div class="container">
+    	<img src="res/titulo.png" class="center" style="margin-bottom: 10%;">
+    </div>
+    <div class="container center_div">
+	    <form>
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Código</label>
+		    <input type="text" class="form-control" name="idCode" id="idCode" placeholder="Ingresa tu código">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Nombres</label>
+		    <input type="text" class="form-control" name="name" id="name" placeholder="Ingresa tu o tus nombres">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Apellidos</label>
+		    <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Ingresa tus dos apellidos">
+		  </div>
+		  <button type="submit" class="btn btn-warning">Verificar</button>
+		</form>
+	</div>
   </body>
 </html>
