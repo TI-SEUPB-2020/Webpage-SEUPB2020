76b8f006469ac8c1374ee393d614223ef5683410
jordiugarte <jordi933@gmail.com>
2020-05-28 12:06:45 -0400

    Everything working

--- final.php
+++ final.php
@@ -1,28 +1,57 @@
-<html lang="en">
-<head>
-  <title>Gracias por votar</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<body>
   <?php
-  	session_start();
+    session_start();
     if (isset($_GET['schoolId']) && isset($_SESSION['idCode']) ) {
-    	echo "".$_GET['schoolId']."";
       $idCode = $_SESSION['idCode'];
       $idSchool = $_GET['schoolId'];
-		include 'db_connection.php';
-		$result = mysqli_query($db_connection, "SELECT voted from registered_students where code='$idCode' and voted='false';");
-  		if ($results['count'] == 1) {
-  			$schoolsSql = mysqli_query($db_connection, "UPDATE schools set votes=votes+1 where id_school=".$idSchool.";");
-  			$votesSql = mysqli_query($db_connection, 'INSERT INTO votes (id_user, id_school, date) VALUES ('.$idCode.', '.$idSchool.', NOW( ));');
-  			$studentsSql = mysqli_query($db_connection, "UPDATE registered_students set voted=1 where code=".$idCode.";");
-  		}
+      include 'db_connection.php';
+      $query = "SELECT voted from registered_students where code='$idCode' and voted='false';";
+      $consult = mysqli_query($db_connection, $query);
+      $result = mysqli_num_rows($consult);
+      if ($result == 1) {
+        $query1 = mysqli_query($db_connection, "UPDATE schools set votes=votes+1 where id_school=".$idSchool.";");
+        $query2 = mysqli_query($db_connection, 'INSERT INTO votes (id_user, id_school, date) VALUES ('.$idCode.', '.$idSchool.', NOW( ));');
+        $query3 = mysqli_query($db_connection, "UPDATE registered_students set voted=1 where code=".$idCode.";");
+      }
     }
     session_destroy();
-    header("Location: http://seupblapaz.com/");
+    //header("Location: http://seupblapaz.com/");
   ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+body {
+  margin: 0;
+  font-family: Arial;
+  background-image:url('res/exitimg.jpg');
+  background-attachment: fixed
+  background-position: center center;
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-size: cover;
+}
+.content {
+  position: fixed;
+  top: 0;
+  background-image: linear-gradient(to bottom, rgba(153, 50, 204,2), rgba(0,0,0,0));
+  color: #f1f1f1;
+  width: 100%;
+  height: 100%;
+
+}
+</style>
+</head>
+<body>
+<div class="content"align="center">
+  <br><br><br><br><br><br>
+<h1 align="center">¡Gracias por participar!</h1>
+<img src="res/seupb.png"style="max-width:30%;height:auto;">
+</div>
+<form align="center" action="index.php">
+  <button type="submit" class="btn btn-warning">Volver</button>
+</form>
+</div>
 </body>
 </html>
